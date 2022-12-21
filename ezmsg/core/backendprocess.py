ea17173979c7291ec7d60674dd0a273d6fc43dd8
Griffin Milsap <griffin.milsap@jhuapl.edu>
2022-12-21 17:21:47 -0500

    debugging an edge case

--- ezmsg/core/backendprocess.py
+++ ezmsg/core/backendprocess.py
@@ -34,14 +34,17 @@ class NormalTermination(Exception):
 class BackendProcess(Process):
     units: List[Unit]
     term_ev: EventType
-    barrier: BarrierType
+    start_barrier: BarrierType
+    stop_barrier: BarrierType
     graph_address: AddressType
 
-    def __init__(self, graph_address: AddressType, units: List[Unit], term_ev: EventType, barrier: BarrierType) -> None:
+    def __init__(self, graph_address: AddressType, units: List[Unit], term_ev: EventType, start_barrier: BarrierType, stop_barrier: BarrierType) -> None:
         super().__init__()
         self.units = units
         self.term_ev = term_ev
-        self.barrier = barrier
+        self.start_barrier = start_barrier
+        self.stop_barrier = stop_barrier
+
         self.graph_address = graph_address
 
 
@@ -132,7 +135,7 @@ class DefaultBackendProcess(BackendProcess):
 
                     process_tasks[f'{unit.address}:{task_name}'] = (unit, task, sub)
 
-            await asyncio.get_running_loop().run_in_executor(None, self.barrier.wait)
+            await asyncio.get_running_loop().run_in_executor(None, self.start_barrier.wait)
 
             tasks: List[asyncio.Task[None]] = [
                 asyncio.create_task(
@@ -147,6 +150,8 @@ class DefaultBackendProcess(BackendProcess):
                 name=f'pid_{self.pid}'
             )
 
+            logger.info(f'Process {self.pid} kicking off tasks: {process_tasks.keys()}')
+
             try:
                 for task in asyncio.as_completed(tasks):
                     # Should raise exceptions in user code
@@ -156,8 +161,11 @@ class DefaultBackendProcess(BackendProcess):
                 monitor.cancel()
                 with suppress(asyncio.CancelledError):
                     await monitor
+        
+            logger.info(f'Process {self.pid} waiting for all other processes')
+            await asyncio.get_running_loop().run_in_executor( None, self.stop_barrier.wait )
 
-        logger.debug(f'Process {self.pid} Completed. All Done: {[task.get_name() for task in tasks]}')
+        logger.info(f'Process {self.pid} Completed. All Done: {[task.get_name() for task in tasks]}')
 
     async def monitor_termination(self, tasks: List[asyncio.Task]):
         while True:
@@ -165,9 +173,13 @@ class DefaultBackendProcess(BackendProcess):
             # await asyncio.get_running_loop().run_in_executor( None, self.term_ev.wait )
             await asyncio.sleep(0.5)
             if self.term_ev.is_set():
-                logger.debug(f'Process {self.pid} detected term_ev; cancelling all tasks')
+                logger.info(f'Process {self.pid} detected term_ev;')
                 for task in tasks:
+                    logger.info(f'Cancelling {task}')
                     task.cancel()
+                    with suppress(asyncio.CancelledError):
+                        await task
+                        logger.info(f'Cancelled {task}')
 
     async def task_wrapper(self, unit: Unit, task: Callable, sub: Optional[Subscriber] = None) -> None:
 
