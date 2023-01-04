072f25591d108c63f9d1ecc6ae75680d814644b0
Griffin Milsap <griffin.milsap@jhuapl.edu>
2023-01-04 16:06:12 -0500

    server control

--- ezmsg/core/__main__.py
+++ ezmsg/core/__main__.py
@@ -1,46 +1,158 @@
 import asyncio
 import argparse
+import socket
+import traceback
 
 from .graphserver import GraphServer
 from .shmserver import SHMServer
-from .netprotocol import AddressType, GRAPHSERVER_ADDR
+from .netprotocol import (
+    Address,
+    GRAPHSERVER_DEFAULT_PORT,
+    GRAPHSERVER_ADDR,
+    SHMSERVER_PORT_ENV,
+)
 
 from . import logger
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+# Port forwarding implementation shamelessly ripped from 
+# https://github.com/medram/port-forwarding/blob/with-asyncio/port_forwarding.py
 
-async def shutdown_servers(address: AddressType = GRAPHSERVER_ADDR) -> None:
-    try:
-        async with GraphServer.connection(address) as connection:
-            connection.shutdown()
-        logger.info(f'Shutdown GraphServer running at {address}')
-    except ConnectionRefusedError:
-        logger.info('GraphServer is not running, or is refusing connections')
+BUFFER_SIZE=4096
+
+async def tunnel(src: socket.socket, dst: socket.socket) -> None:
+    loop = asyncio.get_running_loop()
+    while True:
+        buffer = await loop.sock_recv(src, BUFFER_SIZE)
+        if buffer:
+            await loop.sock_sendall(dst, buffer)
+        else:
+            break
+    logger.info('GraphServer forwarding terminated!')
+
+
+async def accept(server: socket.socket, graph_address: Address):
+    '''Accept server connection'''
+    loop = asyncio.get_running_loop()
+
+    client, addr = await loop.sock_accept(server)
+    client.setblocking(False)
+    # Establishing a connection to destination.
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.setblocking(False)
+    await loop.sock_connect(conn, (graph_address.host, graph_address.port))
+
+    logger.info(f'GraphServer forwarding established: {client.getsockname()} -> ? -> {conn.getpeername()}')
+
+    # piping data
+    loop.create_task(tunnel(client, conn))
+    loop.create_task(tunnel(conn, client))
+
+
+async def forward(local_address: Address, graph_address: Address):
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.setblocking(False)
+        server.bind(local_address)
+        server.listen()
+
+        logger.info(f'Forwarding GraphServer @ {local_address}')
+
+        while True:
+            try:
+                await accept(server, graph_address)
+
+            except ConnectionRefusedError:
+                logger.error('Destination connection refused.')
+            except OSError:
+                logger.error('socket.connect() error!')
+                logger.error(traceback.format_exc())
+            except Exception:
+                logger.error(traceback.format_exc())
+
+
+async def main() -> None:
+    parser = argparse.ArgumentParser(
+        'ezmsg.core',
+        description = 'start and stop core ezmsg server processes',
+        epilog = f"""
+            Change GraphServer and SHMServer ports with the {SHMSERVER_PORT_ENV} 
+            environment variable.  SHMServer will be hosted on {SHMSERVER_PORT_ENV} and 
+            GraphServer will be hosted on {SHMSERVER_PORT_ENV} + 1.  Publishers will be 
+            assigned available ports starting from {SHMSERVER_PORT_ENV} + 2.
+        """
+    )
+
+    parser.add_argument(
+        '-s', '--shutdown', 
+        help = 'shutdown ezmsg core servers', 
+        action = 'store_true'
+    )
+
+    parser.add_argument(
+        '-f', '--forward',
+        help = 'forward ezmsg GraphServer from HOSTNAME',
+        action = 'store_true'
+    )
+
+    parser.add_argument(
+        '--hostname',
+        type = str,
+        help = 'hostname for GraphServer',
+        default = '127.0.0.1'
+    )
+
+    class Args:
+        shutdown: bool
+        forward: bool
+        hostname: str
+
+    args = parser.parse_args(namespace=Args)
+    address = Address(args.hostname, GRAPHSERVER_DEFAULT_PORT)
+
+    if args.shutdown:
+        try:
+            async with GraphServer.connection(address) as connection:
+                connection.shutdown()
+            logger.info(f'Shutdown GraphServer running at {address}')
+        except ConnectionRefusedError:
+            logger.info('GraphServer is not running, or is refusing connections')
+            
+        try:
+            await SHMServer.shutdown_server()
+            logger.info('Shutdown SHMServer')
+        except ConnectionRefusedError:
+            logger.info('SHMServer not running, or is refusing connections')
+
+    else:
+        graph_server = None
+        shm_server = await SHMServer.ensure_running()
+        try:
+            if args.forward:
+                await GraphServer.open(address)
+                await forward(Address(*GRAPHSERVER_ADDR), address)
+            else:
+                graph_server = await GraphServer.ensure_running(address)
+
+                if graph_server is None:
+                    logger.info(f'GraphServer already running @ {address}')
+                else:
+                    graph_server.join()
         
-    try:
-        await SHMServer.shutdown_server()
-        logger.info('Shutdown SHMServer')
-    except ConnectionRefusedError:
-        logger.info('SHMServer not running, or is refusing connections')
+        except KeyboardInterrupt:
+            logger.info('Interrupt detected; shutting down servers')
 
-parser = argparse.ArgumentParser('ezmsg.core')
+        finally:
+            if graph_server is not None:
+                graph_server._shutdown.set()
+                graph_server.join()
 
-parser.add_argument(
-    'command', 
-    type=str, 
-    help = 'command', 
-    choices=['start', 'shutdown']
-)
+            if shm_server is not None:
+                shm_server._shutdown.set()
+                shm_server.join()
+        
 
-class Args:
-    command: str
+asyncio.run(main())
 
-args = parser.parse_args(namespace=Args)
 
-if args.command == 'shutdown':
-    try:
-        loop.run_until_complete(shutdown_servers())
-    finally:
-        loop.close()
     
