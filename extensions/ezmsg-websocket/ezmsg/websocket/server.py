75b348fbe900444b587be9542b8f02337126fc66
Griffin Milsap <griffin.milsap@jhuapl.edu>
2023-01-20 09:25:10 -0500

    hotfix for packaging issue

--- extensions/ezmsg-websocket/ezmsg/websocket/server.py
+++ extensions/ezmsg-websocket/ezmsg/websocket/server.py
@@ -6,7 +6,21 @@ import websockets.exceptions
 
 import ezmsg.core as ez
 
-from . import WebsocketSettings, WebsocketState
+from dataclasses import field
+from typing import Optional, Union
+
+import ezmsg.core as ez
+
+class WebsocketSettings(ez.Settings):
+    host: str
+    port: int
+    cert_path: Optional[str] = None
+
+
+class WebsocketState(ez.State):
+    incoming_queue: "asyncio.Queue[Union[str,bytes]]" = field(default_factory=asyncio.Queue)
+    outgoing_queue: "asyncio.Queue[Union[str,bytes]]" = field(default_factory=asyncio.Queue)
+    
 
 class WebsocketServer(ez.Unit):
     
