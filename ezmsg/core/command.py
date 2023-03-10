2fadc48ac9fe70d328d5b2adca622a20e7b723db
Preston Peranich <pperanich@gmail.com>
2023-03-10 15:26:53 -0500

    Graphviz now functional.

--- ezmsg/core/command.py
+++ ezmsg/core/command.py
@@ -2,6 +2,10 @@ import asyncio
 import argparse
 import logging
 import subprocess
+from collections import defaultdict
+from uuid import uuid4
+from textwrap import indent
+
 
 from .graphserver import GraphServer
 from .shmserver import SHMServer
@@ -16,8 +20,11 @@ from .netprotocol import (
     SHMSERVER_PORT_DEFAULT,
     SHMSERVER_PORT,
 )
+from .dag import DAG
 
 logger = logging.getLogger("ezmsg")
+
+IND = "  "
 
 
 def cmdline() -> None:
@@ -33,24 +40,36 @@ def cmdline() -> None:
         """,
     )
 
-    parser.add_argument(
-        "command",
-        help="command for ezmsg",
-        choices=["serve", "shutdown", "start", "graphviz"],
-    )
+    # parser.add_argument(
+    #     "command",
+    #     help="command for ezmsg",
+    #     choices=["serve", "shutdown", "start", "graphviz"],
+    # )
 
     parser.add_argument(
         "--hostname", type=str, help="hostname for GraphServer", default="127.0.0.1"
     )
-
-    logger.info(f"{GRAPHSERVER_PORT=}; use ${GRAPHSERVER_PORT_ENV} to change.")
-    logger.info(f"{SHMSERVER_PORT=}; use ${SHMSERVER_PORT_ENV} to change.")
+    subparsers = parser.add_subparsers(dest="command", help="command for ezmsg")
+    _ = subparsers.add_parser("serve")
+    _ = subparsers.add_parser("shutdown")
+    _ = subparsers.add_parser("start")
+    graphviz_parser = subparsers.add_parser("graphviz")
+    graphviz_parser.add_argument(
+        "--out",
+        help="file to save graphviz output.",
+        type=str,
+        default=None,
+        required=False,
+    )
 
     class Args:
         command: str
         hostname: str
 
     args = parser.parse_args(namespace=Args)
+
+    logger.info(f"{GRAPHSERVER_PORT=}; use ${GRAPHSERVER_PORT_ENV} to change.")
+    logger.info(f"{SHMSERVER_PORT=}; use ${SHMSERVER_PORT_ENV} to change.")
 
     asyncio.run(
         run_command(args.command, (args.hostname, GRAPHSERVER_PORT), **vars(args))
@@ -113,5 +132,65 @@ async def run_command(
 
         logger.info(f"Forked ezmsg servers.")
     elif cmd == "graphviz":
-        graph_dict = await GraphServer.Connection(address).dag()
-        print(graph_dict)
+        dag: DAG = await GraphServer.Connection(address).dag()
+
+        # Let's come up with UUID node names
+        node_map = {name: f'"{str(uuid4())}"' for name in dag.nodes}
+
+        # Construct the graph
+        def tree():
+            return defaultdict(tree)
+
+        graph: defaultdict = tree()
+
+        connections = ""
+        for node, conns in dag.graph.items():
+            subgraph = graph
+            path = node.split("/")
+            route = path[:-1]
+            stream = path[-1]
+            for seg in route:
+                subgraph = subgraph[seg]
+            subgraph[stream] = node
+
+            for sub in conns:
+                connections += f"{node_map[node]} -> {node_map[sub]};" + "\n"
+
+        # Now convert to dot syntax
+        def recurse_graph(g: defaultdict):
+            out = ""
+            for leaf in g:
+                if type(g[leaf]) == defaultdict:
+                    out += indent(
+                        "\n".join(
+                            [
+                                f"subgraph {leaf.lower()} {{",
+                                indent(f"cluster = true;", IND),
+                                indent(f'label = "{leaf}";', IND),
+                                f"{recurse_graph(g[leaf])}}};",
+                                "",
+                            ]
+                        ),
+                        IND,
+                    )
+                elif type(g[leaf]) == str:
+                    out += indent(f"{node_map[g[leaf]]} [label={leaf}];" + "\n", IND)
+            return out
+
+        subgraph_tree = recurse_graph(graph)
+
+        graphviz_out = "\n".join(
+            [
+                "digraph EZ {",
+                indent('rankdir="LR"', IND),
+                subgraph_tree,
+                indent(connections, IND),
+                "}",
+            ]
+        )
+
+        if "out" in kwargs and kwargs["out"] is not None:
+            with open(kwargs["out"], "w") as fh:
+                fh.write(graphviz_out)
+        else:
+            print(graphviz_out)
