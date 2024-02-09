0132c2188b727db39416f83372b1c8a84c7fc3e9
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-02-08 22:19:35 -0500

    add unit tests for gen_to_unit * moved common func + classes into test utils

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,73 +1,18 @@
+
 import json
 import os
 import pytest
-import tempfile
-
-from pathlib import Path
-from dataclasses import asdict, dataclass
 
 import ezmsg.core as ez
 
-from typing import Optional, AsyncGenerator
-
-# MESSAGE DEFINITIONS
-
-
-@dataclass
-class SimpleMessage:
-    number: float
-
-
-# MESSAGE GENERATOR
-
-
-class MessageGeneratorSettings(ez.Settings):
-    num_msgs: int
-
-
-class MessageGenerator(ez.Unit):
-    SETTINGS: MessageGeneratorSettings
-
-    OUTPUT = ez.OutputStream(SimpleMessage)
-
-    @ez.publisher(OUTPUT)
-    async def spawn(self) -> AsyncGenerator:
-        for i in range(self.SETTINGS.num_msgs):
-            yield self.OUTPUT, SimpleMessage(i)
-        raise ez.Complete
-
-
-# MESSAGE RECEIVER
-
-
-class MessageReceiverSettings(ez.Settings):
-    num_msgs: int
-    output_fn: str
-
-
-class MessageReceiverState(ez.State):
-    num_received: int = 0
-
-
-class MessageReceiver(ez.Unit):
-    STATE: MessageReceiverState
-    SETTINGS: MessageReceiverSettings
-
-    INPUT = ez.InputStream(SimpleMessage)
-
-    @ez.subscriber(INPUT)
-    async def on_message(self, msg: SimpleMessage) -> None:
-        ez.logger.info(f"Msg: {msg}")
-        self.STATE.num_received += 1
-        with open(self.SETTINGS.output_fn, "a") as output_file:
-            output_file.write(json.dumps(asdict(msg)) + "\n")
-        if self.STATE.num_received == self.SETTINGS.num_msgs:
-            raise ez.Complete
+from ez_test_utils import (
+    get_test_fn,
+    MessageGenerator, MessageGeneratorSettings,
+    MessageReceiver, MessageReceiverSettings
+)
 
 
 # Define and configure a system of modules to launch
-
-
 class ToySystemSettings(ez.Settings):
     num_msgs: int
     output_fn: str
@@ -113,26 +58,6 @@ def toy_system_fixture(request):
 
     ToySystem.process_components = func
     return ToySystem
-
-def get_test_fn(test_name: Optional[str] = None, extension: str = "txt") -> Path:
-    """PYTEST compatible temporary test file creator"""
-
-    # Get current test name if we can..
-    if test_name is None:
-        test_name = os.environ.get("PYTEST_CURRENT_TEST")
-        if test_name is not None:
-            test_name = test_name.split(":")[-1].split(" ")[0]
-        else:
-            test_name = __name__
-
-    file_path = Path(tempfile.gettempdir())
-    file_path = file_path / Path(f"{test_name}.{extension}")
-
-    # Create the file
-    with open(file_path, "w"):
-        pass
-
-    return file_path
 
 
 @pytest.mark.parametrize("num_messages", [1, 5, 10])
