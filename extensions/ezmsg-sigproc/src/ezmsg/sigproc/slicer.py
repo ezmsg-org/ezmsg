8296c986aa378d0e8f8978cb9d7261395da82ca6
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-02-13 17:54:26 -0500

    slicer -- single int causes sliced dim to drop.

--- extensions/ezmsg-sigproc/src/ezmsg/sigproc/slicer.py
+++ extensions/ezmsg-sigproc/src/ezmsg/sigproc/slicer.py
@@ -1,5 +1,5 @@
 from dataclasses import replace
-from typing import Generator, Optional
+import typing
 
 import numpy as np
 import ezmsg.core as ez
@@ -12,22 +12,29 @@ Slicer:Select a subset of data along a particular axis.
 """
 
 
-def parse_slice(s: str) -> tuple[slice]:
+def parse_slice(s: str) -> typing.Tuple[typing.Union[slice, int], ...]:
     """
     Parses a string representation of a slice and returns a tuple of slice objects.
+    * "" -> slice(None, None, None)  (take all)
+    * ":" -> slice(None, None, None)
+    * '"none"` (case-insensitive) -> slice(None, None, None)
+    * "{start}:{stop}" or {start}:{stop}:{step} -> slice(start, stop, step)
+    * "5" (or any integer) -> (5,). Take only that item.
+        applying this to a ndarray or AxisArray will drop the dimension.
+    * A comma-separated list of the above -> a tuple of slices | ints
 
     Args:
         s (str): The string representation of the slice.
 
     Returns:
-        tuple[slice]: A tuple of slice objects.
+        tuple[slice | int, ...]: A tuple of slice objects and/or ints.
     """
     if s.lower() in ["", ":", "none"]:
         return (slice(None),)
     if "," not in s:
         parts = [part.strip() for part in s.split(":")]
         if len(parts) == 1:
-            return (slice(int(parts[0]), int(parts[0]) + 1),)
+            return (int(parts[0]),)
         return (slice(*(int(part.strip()) if part else None for part in parts)),)
     l = [parse_slice(_) for _ in s.split(",")]
     return tuple([item for sublist in l for item in sublist])
@@ -35,11 +42,12 @@ def parse_slice(s: str) -> tuple[slice]:
 
 @consumer
 def slicer(
-    selection: str = "", axis: Optional[str] = None
-) -> Generator[AxisArray, AxisArray, None]:
+    selection: str = "", axis: typing.Optional[str] = None
+) -> typing.Generator[AxisArray, AxisArray, None]:
     axis_arr_in = AxisArray(np.array([]), dims=[""])
     axis_arr_out = AxisArray(np.array([]), dims=[""])
     _slice = None
+    b_change_dims = False
 
     while True:
         axis_arr_in = yield axis_arr_out
@@ -52,20 +60,33 @@ def slicer(
             _slices = parse_slice(selection)
             if len(_slices) == 1:
                 _slice = _slices[0]
+                b_change_dims = isinstance(_slice, int)
             else:
+                # Multiple slices, but this cannot be done in a single step, so we convert the slices
+                #  to a discontinuous set of integer indexes.
                 indices = np.arange(axis_arr_in.data.shape[axis_idx])
                 indices = np.hstack([indices[_] for _ in _slices])
                 _slice = np.s_[indices]
 
+        if b_change_dims:
+            out_dims = [_ for dim_ix, _ in enumerate(axis_arr_in.dims) if dim_ix != axis_idx]
+            out_axes = axis_arr_in.axes.copy()
+            out_axes.pop(axis, None)
+        else:
+            out_dims = axis_arr_in.dims
+            out_axes = axis_arr_in.axes
+
         axis_arr_out = replace(
             axis_arr_in,
+            dims=out_dims,
+            axes=out_axes,
             data=slice_along_axis(axis_arr_in.data, _slice, axis_idx),
         )
 
 
 class SlicerSettings(ez.Settings):
     selection: str = ""
-    axis: Optional[str] = None
+    axis: typing.Optional[str] = None
 
 
 class Slicer(GenAxisArray):
