596cf620787c233ded42853db4402a15bba25b3c
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-02-15 23:01:04 -0500

    Refactor Counter and SinGenerator as generators. Add unit tests.

--- extensions/ezmsg-sigproc/tests/test_synth.py
+++ extensions/ezmsg-sigproc/tests/test_synth.py
@@ -1,3 +1,4 @@
+import asyncio
 from dataclasses import field
 import os
 import time
@@ -14,7 +15,8 @@ from ezmsg.util.terminate import TerminateOnTotalSettings, TerminateOnTotal
 from util import get_test_fn
 from ezmsg.sigproc.synth import (
     clock, aclock, Clock, ClockSettings,
-    Counter, CounterSettings
+    acounter, Counter, CounterSettings,
+    sin, SinGenerator, SinGeneratorSettings
 )
 
 
@@ -106,6 +108,108 @@ def test_clock_system(
 
 
 # TEST COUNTER #
+@pytest.mark.parametrize("mode", ["interleaved", "blockwise"])
+@pytest.mark.asyncio
+async def test_acounter_external(
+    mode: str,
+    n_time: int = 8,
+    fs: float = 72.0,
+    n_ch: int = 3
+):
+    assert mode in ["interleaved", "blockwise"]
+    n_clocks = 20
+
+    agen = acounter(n_time=n_time, fs=fs, n_ch=n_ch, dispatch_rate="ext_clock", mod=None)
+
+    # First attempts yield nothing.
+    for _ in range(3):
+        assert await anext(agen) is None
+
+    results = []
+    if mode == "blockwise":
+        for _ in range(n_clocks):
+            tmp = await agen.asend(ez.Flag())
+            await asyncio.sleep(0.0099)
+        results = [await anext(agen) for _ in range(n_clocks + 1)]
+        assert results[-1] is None
+        results = results[:-1]
+    elif mode == "interleaved":
+        for _ in range(n_clocks):
+            _ = await agen.asend(ez.Flag())
+            res = await anext(agen)
+            results.append(res)
+            await asyncio.sleep(0.009)
+
+    for ax_arr in results:
+        assert type(ax_arr) is AxisArray
+        assert ax_arr.data.shape == (n_time, n_ch)
+        assert "time" in ax_arr.axes
+        assert ax_arr.axes["time"].gain == 1 / fs
+
+    offsets = np.array([_.axes["time"].offset for _ in results])
+    deltas = np.diff(offsets)
+    assert np.logical_and(0.009 < deltas, deltas < 0.012).all()
+
+    agg = AxisArray.concatenate(*results, dim="time")
+    assert np.array_equal(agg.data[:, 0], np.arange(n_clocks * n_time))
+
+
+@pytest.mark.parametrize("block_size", [1, 20])
+@pytest.mark.parametrize("fs", [10.0, 1000.0])
+@pytest.mark.parametrize("n_ch", [3])
+@pytest.mark.parametrize("dispatch_rate", [None, "realtime", 2.0, 20.0])  # "ext_clock" needs a separate test
+@pytest.mark.parametrize("mod", [2 ** 3, None])
+@pytest.mark.asyncio
+async def test_acounter(
+    block_size: int,
+    fs: float,
+    n_ch: int,
+    dispatch_rate: typing.Optional[typing.Union[float, str]],
+    mod: typing.Optional[int]
+):
+    assert dispatch_rate != "ext_clock"
+
+    target_dur = 2.6  # 2.6 seconds per test
+    if dispatch_rate is None:
+        # No sleep / wait
+        chunk_dur = 0.1
+    elif dispatch_rate == "realtime":
+        chunk_dur = block_size / fs
+    else:
+        # Note: float dispatch_rate will yield different number of samples than expected by target_dur and fs
+        chunk_dur = 1. / dispatch_rate
+    target_messages = int(target_dur / chunk_dur)
+
+    # Run generator
+    agen = acounter(block_size, fs, n_ch=n_ch, dispatch_rate=dispatch_rate, mod=mod)
+    messages = [await anext(agen) for _ in range(target_messages)]
+
+    # Test contents of individual messages
+    for ax_arr in messages:
+        assert type(ax_arr) is AxisArray
+        assert ax_arr.data.shape == (block_size, n_ch)
+        assert "time" in ax_arr.axes
+        assert ax_arr.axes["time"].gain == 1 / fs
+
+    agg = AxisArray.concatenate(*messages, dim="time")
+
+    target_samples = block_size * target_messages
+    expected_data = np.arange(target_samples)
+    if mod is not None:
+        expected_data = expected_data % mod
+    assert np.array_equal(agg.data[:, 0], expected_data)
+
+    offsets = np.array([_.axes["time"].offset for _ in messages])
+    expected_offsets = np.arange(target_messages) * block_size / fs
+    if dispatch_rate is not None and dispatch_rate == "realtime":
+        expected_offsets += offsets[0]  # offsets are in real-time
+        atol = 0.002
+    else:
+        # Offsets are synthetic.
+        atol = 1.e-8
+    assert np.allclose(offsets[2:], expected_offsets[2:], atol=atol)
+
+
 class CounterTestSystemSettings(ez.Settings):
     counter_settings: CounterSettings
     log_settings: MessageLoggerSettings
@@ -131,19 +235,27 @@ class CounterTestSystem(ez.Collection):
         )
 
 
-@pytest.mark.parametrize("block_size", [1, 20])
-@pytest.mark.parametrize("fs", [10.0, 1000.0])
-@pytest.mark.parametrize("n_ch", [3])
-@pytest.mark.parametrize("dispatch_rate", [None, "realtime", 2.0, 20.0])  # "ext_clock" needs a separate test
-@pytest.mark.parametrize("mod", [2**3, None])
+# Integration Test.
+# General functionality of acounter verified above. Here we only need to test a couple configs.
+@pytest.mark.parametrize(
+    "block_size, fs, dispatch_rate, mod",
+    [
+        (1, 10.0, None, None),
+        (20, 1000.0, "realtime", None),
+        (1, 1000.0, 2.0, 2**3),
+        (10, 10.0, 20.0, 2**3),
+        # No test for ext_clock because that requires a different system
+        # (20, 10.0, "ext_clock", None),
+    ]
+)
 def test_counter_system(
         block_size: int,
         fs: float,
-        n_ch: int,
         dispatch_rate: typing.Optional[typing.Union[float, str]],
         mod: typing.Optional[int],
         test_name: typing.Optional[str] = None,
 ):
+    n_ch = 3
     target_dur = 2.6  # 2.6 seconds per test
     if dispatch_rate is None:
         # No sleep / wait
@@ -189,22 +301,45 @@ def test_counter_system(
         messages = messages[:target_messages]
     assert len(messages) == target_messages
 
+    # Just do one quick data check
+    agg = AxisArray.concatenate(*messages, dim="time")
     target_samples = block_size * target_messages
-    time_idx, ch_idx = messages[0].get_axis_idx("time"), messages[0].get_axis_idx("ch")
-    data = np.concatenate([_.data for _ in messages], axis=time_idx)
-    assert data.shape[0] == target_samples
-    assert data.shape[ch_idx] == n_ch
     expected_data = np.arange(target_samples)
     if mod is not None:
         expected_data = expected_data % mod
-    assert np.array_equal(data[:, 0], expected_data)
+    assert np.array_equal(agg.data[:, 0], expected_data)
 
-    offsets = np.array([_.axes["time"].offset for _ in messages])
-    expected_offsets = np.arange(target_messages) * block_size / fs
-    if dispatch_rate is not None and dispatch_rate == "realtime":
-        expected_offsets += offsets[0]  # offsets are in real-time
-        atol = 0.002
-    else:
-        # Offsets are synthetic.
-        atol = 1.e-8
-    assert np.allclose(offsets[2:], expected_offsets[2:], atol=atol)
+
+# TEST SIN #
+def test_sin_gen(
+        freq: float = 1.0,
+        amp: float = 1.0,
+        phase: float = 0.0
+):
+    axis: typing.Optional[str] = "time"
+    srate = max(4.0 * freq, 1000.0)
+    sim_dur = 30.0
+    n_samples = int(srate * sim_dur)
+    n_msgs = min(n_samples, 10)
+    axis_idx = 0
+
+    messages = []
+    for split_dat in np.array_split(np.arange(n_samples)[:, None], n_msgs, axis=axis_idx):
+        _time_axis = AxisArray.Axis.TimeAxis(fs=srate, offset=float(split_dat[0, 0]))
+        messages.append(
+            AxisArray(split_dat, dims=["time", "ch"], axes={"time": _time_axis})
+        )
+
+    def f_test(t): return amp * np.sin(2 * np.pi * freq * t + phase)
+
+    gen = sin(axis=axis, freq=freq, amp=amp, phase=phase)
+    results = []
+    for msg in messages:
+        res = gen.send(msg)
+        assert np.allclose(res.data, f_test(msg.data / srate))
+        results.append(res)
+    concat_ax_arr = AxisArray.concatenate(*results, dim="time")
+    assert np.allclose(concat_ax_arr.data, f_test(np.arange(n_samples) / srate)[:, None])
+
+
+# TODO: test SinGenerator in a system.
