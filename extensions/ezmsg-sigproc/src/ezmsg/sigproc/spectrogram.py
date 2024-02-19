48309953d4df6484bfaa61fa98122ec6fb90cb1d
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-02-19 11:15:53 -0500

    spectrogram unit use generator

--- extensions/ezmsg-sigproc/src/ezmsg/sigproc/spectrogram.py
+++ extensions/ezmsg-sigproc/src/ezmsg/sigproc/spectrogram.py
@@ -1,15 +1,14 @@
-from dataclasses import field, replace
 import typing
 
 import numpy as np
 
 import ezmsg.core as ez
 from ezmsg.util.messages.axisarray import AxisArray
-from ezmsg.util.generator import compose, consumer, GenAxisArray
-from ezmsg.util.messages.modify import modify_axis, ModifyAxis, ModifyAxisSettings
-from ezmsg.sigproc.window import windowing, Window, WindowSettings
+from ezmsg.util.generator import consumer, GenAxisArray  # , compose
+from ezmsg.util.messages.modify import modify_axis
+from ezmsg.sigproc.window import windowing
 from ezmsg.sigproc.spectrum import (
-    spectrum, Spectrum, SpectrumSettings,
+    spectrum,
     WindowFunction, SpectralTransform, SpectralOutput
 )
 
@@ -23,6 +22,8 @@ def spectrogram(
     output: SpectralOutput = SpectralOutput.POSITIVE
 ) -> typing.Generator[typing.Optional[AxisArray], AxisArray, None]:
 
+    # We cannot use `compose` because `windowing` returns a list of axisarray objects,
+    #  even though the length is always exactly 1 for the settings used here.
     # pipeline = compose(
     f_win = windowing(axis="time", newaxis="step", window_dur=window_dur, window_shift=window_shift)
     f_spec = spectrum(axis="time", window=window, transform=transform, output=output)
@@ -36,60 +37,32 @@ def spectrogram(
     while True:
         axis_arr_in = yield axis_arr_out
 
+        # axis_arr_out = pipeline(axis_arr_in)
         axis_arr_out = None
         wins = f_win.send(axis_arr_in)
         if len(wins):
             specs = f_spec.send(wins[0])
             if specs is not None:
                 axis_arr_out = f_modify.send(specs)
-        # axis_arr_out = pipeline(axis_arr_in)
 
 
 class SpectrogramSettings(ez.Settings):
-    window_dur: float | None = None  # window duration in seconds
-    window_shift: float | None = None  # window step in seconds. If None, window_shift == window_dur
+    window_dur: typing.Optional[float] = None  # window duration in seconds
+    window_shift: typing.Optional[float] = None  # window step in seconds. If None, window_shift == window_dur
     # See SpectrumSettings for details of following settings:
     window: WindowFunction = WindowFunction.HAMMING
     transform: SpectralTransform = SpectralTransform.REL_DB
     output: SpectralOutput = SpectralOutput.POSITIVE
 
 
-class SpectrogramState(ez.State):
-    cur_settings: SpectrogramSettings
-
-
-class Spectrogram(ez.Collection):
+class Spectrogram(GenAxisArray):
     SETTINGS: SpectrogramSettings
-    STATE: SpectrogramState
 
-    # Ports
-    INPUT_SIGNAL = ez.InputStream(AxisArray)
-    OUTPUT_SIGNAL = ez.OutputStream(AxisArray)
-
-    # Subunits
-    WINDOW = Window()
-    SPECTRUM = Spectrum()
-    MODIFYAXIS = ModifyAxis()
-
-    def configure(self) -> None:
-        self.WINDOW.apply_settings(WindowSettings(
-            axis="time",
-            newaxis="step",
+    def construct_generator(self):
+        self.STATE.gen = spectrogram(
             window_dur=self.SETTINGS.window_dur,
-            window_shift=self.SETTINGS.window_shift
-        ))
-        self.SPECTRUM.apply_settings(SpectrumSettings(
-            axis="time",
+            window_shift=self.SETTINGS.window_shift,
             window=self.SETTINGS.window,
             transform=self.SETTINGS.transform,
             output=self.SETTINGS.output
-        ))
-        self.MODIFYAXIS.apply_settings(ModifyAxisSettings(name_map={"step": "time"}))
-
-    def network(self) -> ez.NetworkDefinition:
-        return (
-            (self.INPUT_SIGNAL, self.WINDOW.INPUT_SIGNAL),
-            (self.WINDOW.OUTPUT_SIGNAL, self.SPECTRUM.INPUT_SIGNAL),
-            (self.SPECTRUM.OUTPUT_SIGNAL, self.MODIFYAXIS.INPUT_SIGNAL),
-            (self.MODIFYAXIS.OUTPUT_SIGNAL, self.OUTPUT_SIGNAL)
         )
