a56338047c1bc6564cf4ee56192e5e7540f5e1e4
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-02-19 11:15:16 -0500

    Add create_messages_with_periodic_signal to test util.

--- extensions/ezmsg-sigproc/tests/test_spectrum.py
+++ extensions/ezmsg-sigproc/tests/test_spectrum.py
@@ -1,46 +1,9 @@
-import os
-import json
-import typing
-
 import pytest
 import numpy as np
-from numpy.lib.stride_tricks import sliding_window_view
 
-import ezmsg.core as ez
 from ezmsg.util.messages.axisarray import AxisArray, slice_along_axis
-from ezmsg.util.messagegate import MessageGate, MessageGateSettings
-from ezmsg.util.messagelogger import MessageLogger, MessageLoggerSettings
-from ezmsg.util.messagecodec import message_log
-from ezmsg.sigproc.synth import sin
 from ezmsg.sigproc.spectrum import spectrum, SpectralTransform, SpectralOutput, WindowFunction
-
-from util import get_test_fn
-from ezmsg.util.terminate import TerminateOnTimeout as TerminateTest
-from ezmsg.util.terminate import TerminateOnTimeoutSettings as TerminateTestSettings
-
-
-def make_win_messages(win_dur, win_step_dur, fs, afps, test_dur=20.0) -> typing.List[AxisArray]:
-    # Prepare inputs
-    win_len = int(win_dur * fs)
-    win_step = int(win_step_dur * fs)
-    t = np.arange(int(test_dur * fs)) / fs
-    data = np.sum(np.vstack(
-        [a * np.sin(2 * np.pi * f * t + p) for a, f, p in afps]
-    ), axis=0)
-    win_data = sliding_window_view(data, (win_len,), axis=0)[::win_step]
-
-    messages = []
-    offset = 0
-    for win_ix, dat in enumerate(win_data):
-        _time_axis = AxisArray.Axis.TimeAxis(fs=fs, offset=offset)
-        messages.append(AxisArray(
-            dat[..., None],
-            dims=["time", "ch"],
-            axes={"time": _time_axis}
-        ))
-        offset += dat.shape[0] / fs
-
-    return messages
+from util import get_test_fn, create_messages_with_periodic_signal
 
 
 def _debug_plot_welch(raw: AxisArray, result: AxisArray, welch_db: bool = True):
@@ -88,9 +51,19 @@ def test_spectrum_gen_multiwin(
     win_dur = 1.0
     win_step_dur = 0.5
     fs = 1000.0
-    afps = [(1.0, 10.0, 0.0), (0.5, 20.0, np.pi / 7), (0.2, 200.0, np.pi / 11)]
+    sin_params = [
+        {"a": 1.0, "f": 10.0, "p": 0.0, "dur": 20.0},
+        {"a": 0.5, "f": 20.0, "p": np.pi / 7, "dur": 20.0},
+        {"a": 0.2, "f": 200.0, "p": np.pi / 11, "dur": 20.0},
+    ]
     win_len = int(win_dur * fs)
-    messages = make_win_messages(win_dur, win_step_dur, fs, afps)
+
+    messages = create_messages_with_periodic_signal(
+        sin_params=sin_params,
+        fs=fs,
+        msg_dur=win_dur,
+        win_step_dur=win_step_dur
+    )
     input_multiwin = AxisArray.concatenate(*messages, dim="win")
     input_multiwin.axes["win"] = AxisArray.Axis.TimeAxis(offset=0, fs=1/win_step_dur)
 
@@ -111,8 +84,8 @@ def test_spectrum_gen_multiwin(
     f_vec = result.axes["freq"].gain * np.arange(f_len) + result.axes["freq"].offset
     if output == SpectralOutput.NEGATIVE:
         f_vec = np.abs(f_vec)
-    for a, f, p in afps:
-        f_ix = np.argmin(np.abs(f_vec - f))
+    for s_p in sin_params:
+        f_ix = np.argmin(np.abs(f_vec - s_p["f"]))
         peak_inds = np.argmax(slice_along_axis(result.data, slice(f_ix-3, f_ix+3), axis=fax_ix), axis=fax_ix)
         assert np.all(peak_inds == 3)
 
@@ -128,9 +101,17 @@ def test_spectrum_gen(
     win_dur = 1.0
     win_step_dur = 0.5
     fs = 1000.0
-    afps = [(1.0, 10.0, 0.0), (0.5, 20.0, np.pi / 7), (0.2, 200.0, np.pi / 11)]
-    win_len = int(win_dur * fs)
-    messages = make_win_messages(win_dur, win_step_dur, fs, afps)
+    sin_params = [
+        {"a": 1.0, "f": 10.0, "p": 0.0, "dur": 20.0},
+        {"a": 0.5, "f": 20.0, "p": np.pi / 7, "dur": 20.0},
+        {"a": 0.2, "f": 200.0, "p": np.pi / 11, "dur": 20.0},
+    ]
+    messages = create_messages_with_periodic_signal(
+        sin_params=sin_params,
+        fs=fs,
+        msg_dur=win_dur,
+        win_step_dur=win_step_dur
+    )
     gen = spectrum(axis="time", window=window, transform=transform, output=output)
     results = [gen.send(msg) for msg in messages]
 
