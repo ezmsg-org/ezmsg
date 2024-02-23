d1adb13ba041a49cf215951182583c7bc30a451a
Preston Peranich <pperanich@gmail.com>
2024-02-23 10:26:52 -0500

    Reworked codec to only add timestamp to top level object.

--- src/ezmsg/util/messagecodec.py
+++ src/ezmsg/util/messagecodec.py
@@ -24,6 +24,7 @@ NDARRAY_DTYPE = "dtype"
 NDARRAY_SHAPE = "shape"
 NDARRAY_DATA = "data"
 
+
 @dataclass
 class LogStart:
     ...
@@ -49,10 +50,9 @@ def import_type(typestr: str) -> type:
 class MessageEncoder(json.JSONEncoder):
     def default(self, obj: typing.Any):
         if is_dataclass(obj):
-            ts = getattr(obj, TIMESTAMP_ATTR, time.time())
             return {
-                **{f.name: getattr(obj, f.name) for f in fields(obj)}, 
-                **{TYPE: type_str(obj), TIMESTAMP_ATTR: ts}
+                **{f.name: getattr(obj, f.name) for f in fields(obj)},
+                **{TYPE: type_str(obj)},
             }
 
         elif np and isinstance(obj, np.ndarray):
@@ -67,7 +67,7 @@ class MessageEncoder(json.JSONEncoder):
 
         try:
             return json.JSONEncoder.default(self, obj)
-        
+
         except TypeError:
             buf = base64.b64encode(pickle.dumps(obj))
             return {
@@ -75,24 +75,27 @@ class MessageEncoder(json.JSONEncoder):
                 PICKLE_DATA: buf.decode("ascii"),
             }
 
+
 class StampedMessage(typing.NamedTuple):
     msg: typing.Any
     timestamp: typing.Optional[float]
 
-class MessageTimestampDecoder(json.JSONDecoder):
+
+class MessageDecoder(json.JSONDecoder):
     def __init__(self, *args, **kwargs):
         json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
 
     def object_hook(self, obj: typing.Dict[str, typing.Any]) -> typing.Any:
-        obj_type: typing.Optional[str] = obj.get(TYPE, None)
-        timestamp: typing.Optional[float] = obj.get(TIMESTAMP_ATTR, None)
+        obj_type: typing.Optional[str] = obj.get(TYPE)
 
         out_obj: typing.Any = obj
 
         if obj_type is not None:
             if np and obj_type == NDARRAY_TYPE:
                 data_bytes: typing.Optional[str] = obj.get(NDARRAY_DATA)
-                data_shape: typing.Optional[typing.Iterable[int]] = obj.get(NDARRAY_SHAPE)
+                data_shape: typing.Optional[typing.Iterable[int]] = obj.get(
+                    NDARRAY_SHAPE
+                )
                 data_dtype: typing.Optional[npt.DTypeLike] = obj.get(NDARRAY_DTYPE)
 
                 if (
@@ -102,7 +105,7 @@ class MessageTimestampDecoder(json.JSONDecoder):
                 ):
                     buf = base64.b64decode(data_bytes.encode("ascii"))
                     out_obj = np.frombuffer(buf, dtype=data_dtype).reshape(data_shape)
-                
+
             elif obj_type == PICKLE_TYPE:
                 data_bytes: typing.Optional[str] = obj.get(PICKLE_DATA)
                 if isinstance(data_bytes, str):
@@ -112,22 +115,10 @@ class MessageTimestampDecoder(json.JSONDecoder):
             else:
                 cls = import_type(obj_type)
                 del obj[TYPE]
-                if TIMESTAMP_ATTR in obj: 
-                    del obj[TIMESTAMP_ATTR]
                 out_obj = cls(**obj)
-                if timestamp is not None:
-                    setattr(out_obj, TIMESTAMP_ATTR, timestamp)
 
         return out_obj
-    
-class MessageDecoder(MessageTimestampDecoder):
-    """ Just in case there are side-effects from adding the timestamp attribute """
-    def object_hook(self, obj: typing.Dict[str, typing.Any]) -> typing.Any:
-        obj = super().object_hook(obj)
-        if hasattr(obj, TIMESTAMP_ATTR):
-            delattr(obj, TIMESTAMP_ATTR)
-        return obj
-    
+
 
 def message_log(fname: Path) -> typing.Generator[typing.Any, None, None]:
     with open(fname, "r") as f:
