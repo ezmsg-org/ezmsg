1949473094105f91bceaca11552a0b795028ca0a
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-05-22 23:12:28 -0400

    Upgrade downsample * faster unit tests on core processing * accommodate size-0 arrays * slight optim. using template struct.

--- extensions/ezmsg-sigproc/tests/test_downsample.py
+++ extensions/ezmsg-sigproc/tests/test_downsample.py
@@ -9,7 +9,7 @@ from ezmsg.util.messages.axisarray import AxisArray
 from ezmsg.util.messagegate import MessageGate, MessageGateSettings
 from ezmsg.util.messagelogger import MessageLogger, MessageLoggerSettings
 from ezmsg.util.messagecodec import message_log
-from ezmsg.sigproc.downsample import Downsample, DownsampleSettings
+from ezmsg.sigproc.downsample import downsample, Downsample, DownsampleSettings
 from ezmsg.sigproc.synth import Counter, CounterSettings
 
 from util import get_test_fn
@@ -18,6 +18,50 @@ from ezmsg.util.terminate import TerminateOnTimeoutSettings as TerminateTestSett
 from ezmsg.util.debuglog import DebugLog
 
 from typing import Optional, List
+
+
+@pytest.mark.parametrize("block_size", [1, 5, 10, 20])
+@pytest.mark.parametrize("factor", [1, 2, 3])
+def test_downsample_core(block_size: int, factor: int):
+    in_fs = 19.0
+    test_dur = 4.0
+    n_channels = 2
+    n_features = 3
+    num_samps = int(np.ceil(test_dur * in_fs))
+    num_msgs = int(np.ceil(num_samps / block_size))
+    sig = np.arange(num_samps * n_channels * n_features).reshape(num_samps, n_channels, n_features)
+    tvec = np.arange(num_samps) / in_fs
+
+    def msg_generator():
+        for msg_ix in range(num_msgs):
+            msg_sig = sig[msg_ix*block_size:(msg_ix+1)*block_size]
+            msg_idx = msg_sig[0, 0, 0] / (n_channels * n_features)
+            msg_offs = msg_idx / in_fs
+            msg = AxisArray(
+                data=msg_sig,
+                dims=["time", "ch", "feat"],
+                axes={"time": AxisArray.Axis.TimeAxis(fs=in_fs, offset=msg_offs)}
+            )
+            yield msg
+
+    proc = downsample(axis="time", factor=factor)
+    out_msgs = []
+    for msg in msg_generator():
+        res = proc.send(msg)
+        if res.data.size:
+            out_msgs.append(res)
+
+    # Assert correctness of gain
+    assert all(msg.axes["time"].gain == factor / in_fs for msg in out_msgs)
+
+    # Assert messages have the correct timestamps
+    expected_offsets = np.cumsum([0] + [_.data.shape[0] for _ in out_msgs]) * factor / in_fs
+    actual_offsets = np.array([_.axes["time"].offset for _ in out_msgs])
+    assert np.allclose(actual_offsets, expected_offsets[:-1])
+
+    # Compare returned values to expected values.
+    allres_msg = AxisArray.concatenate(*out_msgs, dim="time")
+    assert np.array_equal(allres_msg.data, sig[::factor])
 
 
 class DownsampleSystemSettings(ez.Settings):
@@ -65,8 +109,8 @@ class DownsampleSystem(ez.Collection):
         )
 
 
-@pytest.mark.parametrize("block_size", [1, 5, 10, 20])
-@pytest.mark.parametrize("factor", [1, 2, 3])
+@pytest.mark.parametrize("block_size", [10])
+@pytest.mark.parametrize("factor", [3])
 def test_downsample_system(
     block_size: int, factor: int, test_name: Optional[str] = None
 ):
