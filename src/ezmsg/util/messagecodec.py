4832632eaab0e70a011360998e5cf6faef856ff1
Konrad Pilch <konradkpilch@gmail.com>
2025-06-26 13:46:55 -0400

    messagecodec object_hook change

--- src/ezmsg/util/messagecodec.py
+++ src/ezmsg/util/messagecodec.py
@@ -46,28 +46,28 @@ def import_type(typestr: str) -> type:
 
 
 class MessageEncoder(json.JSONEncoder):
-    def default(self, obj: typing.Any):
-        if is_dataclass(obj):
+    def default(self, o: typing.Any):
+        if is_dataclass(o):
             return {
-                **{f.name: getattr(obj, f.name) for f in fields(obj)},
-                **{TYPE: type_str(obj)},
+                **{f.name: getattr(o, f.name) for f in fields(o)},
+                **{TYPE: type_str(o)},
             }
 
-        elif np and isinstance(obj, np.ndarray):
-            contig_obj = np.ascontiguousarray(obj)
+        elif np and isinstance(o, np.ndarray):
+            contig_obj = np.ascontiguousarray(o)
             buf = base64.b64encode(contig_obj.data)
             return {
                 TYPE: NDARRAY_TYPE,
-                NDARRAY_DTYPE: str(obj.dtype),
+                NDARRAY_DTYPE: str(o.dtype),
                 NDARRAY_DATA: buf.decode("ascii"),
-                NDARRAY_SHAPE: obj.shape,
+                NDARRAY_SHAPE: o.shape,
             }
 
         try:
-            return json.JSONEncoder.default(self, obj)
+            return json.JSONEncoder.default(self, o)
 
         except TypeError:
-            buf = base64.b64encode(pickle.dumps(obj))
+            buf = base64.b64encode(pickle.dumps(o))
             return {
                 TYPE: PICKLE_TYPE,
                 PICKLE_DATA: buf.decode("ascii"),
@@ -79,43 +79,46 @@ class StampedMessage(typing.NamedTuple):
     timestamp: typing.Optional[float]
 
 
+def _object_hook(obj: dict[str, typing.Any]) -> typing.Any:
+    obj_type: typing.Optional[str] = obj.get(TYPE)
+
+    out_obj: typing.Any = obj
+
+    if obj_type is not None:
+        if np and obj_type == NDARRAY_TYPE:
+            data_bytes: typing.Optional[str] = obj.get(NDARRAY_DATA)
+            data_shape: typing.Optional[typing.Iterable[int]] = obj.get(
+                NDARRAY_SHAPE
+            )
+            data_dtype: typing.Optional[npt.DTypeLike] = obj.get(NDARRAY_DTYPE)
+
+            if (
+                isinstance(data_bytes, str)
+                and data_shape is not None
+                and data_dtype is not None
+            ):
+                buf = base64.b64decode(data_bytes.encode("ascii"))
+                out_obj = np.frombuffer(buf, dtype=data_dtype).reshape(data_shape)
+
+        elif obj_type == PICKLE_TYPE:
+            data_bytes: typing.Optional[str] = obj.get(PICKLE_DATA)
+            if isinstance(data_bytes, str):
+                buf = base64.b64decode(data_bytes.encode("ascii"))
+                out_obj = pickle.loads(buf)
+
+        else:
+            cls = import_type(obj_type)
+            del obj[TYPE]
+            out_obj = cls(**obj)
+
+    return out_obj
+
+
 class MessageDecoder(json.JSONDecoder):
     def __init__(self, *args, **kwargs):
-        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+        json.JSONDecoder.__init__(self, object_hook=_object_hook, *args, **kwargs)
 
-    def object_hook(self, obj: typing.Dict[str, typing.Any]) -> typing.Any:
-        obj_type: typing.Optional[str] = obj.get(TYPE)
 
-        out_obj: typing.Any = obj
-
-        if obj_type is not None:
-            if np and obj_type == NDARRAY_TYPE:
-                data_bytes: typing.Optional[str] = obj.get(NDARRAY_DATA)
-                data_shape: typing.Optional[typing.Iterable[int]] = obj.get(
-                    NDARRAY_SHAPE
-                )
-                data_dtype: typing.Optional[npt.DTypeLike] = obj.get(NDARRAY_DTYPE)
-
-                if (
-                    isinstance(data_bytes, str)
-                    and data_shape is not None
-                    and data_dtype is not None
-                ):
-                    buf = base64.b64decode(data_bytes.encode("ascii"))
-                    out_obj = np.frombuffer(buf, dtype=data_dtype).reshape(data_shape)
-
-            elif obj_type == PICKLE_TYPE:
-                data_bytes: typing.Optional[str] = obj.get(PICKLE_DATA)
-                if isinstance(data_bytes, str):
-                    buf = base64.b64decode(data_bytes.encode("ascii"))
-                    out_obj = pickle.loads(buf)
-
-            else:
-                cls = import_type(obj_type)
-                del obj[TYPE]
-                out_obj = cls(**obj)
-
-        return out_obj
 
 
 def message_log(
